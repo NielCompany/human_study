57d56c25616ff2b3b1d6d4f40ce17fc76e327a03
kyle <siz3841@gmail.com>
2025-05-29 10:54:20 +0900

    classes update

--- human_study/defect_checker_code/app/src/model/predict.py
+++ human_study/defect_checker_code/app/src/model/predict.py
@@ -4,15 +4,21 @@ from torchvision import transforms, models, datasets
 from PIL import Image
 import os
 
-# ✅ 클래스 정보 (ImageFolder 기준)
-train_dataset = datasets.ImageFolder('./dataset/train')
-classes = train_dataset.classes
+# # ✅ 클래스 정보 (ImageFolder 기준)
+# train_dataset = datasets.ImageFolder('./dataset/train')
+# classes = train_dataset.classes
+
+classes = [
+    "개폐 불량","고정 불량","균열","누수","도배 불량","도장 불량","랩핑 불량","마감 불량","마루 불량",
+    "문틀 불량","벽체 불량","수직 및 수평 불량","수직 수평 불량","수평 불량","시공 불량",
+    "시공 확인 필요","없음","오염","자재 불량","줄눈 마감 불량","코킹 불량","타공 불량","타일 불량"
+]
 
 # ✅ 모델 준비
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = models.resnet50(weights=None)
 model.fc = torch.nn.Linear(model.fc.in_features, len(classes))
-model.load_state_dict(torch.load('best_resnet50.pth', map_location=device))
+model.load_state_dict(torch.load('models/resnet50_yolov5_acc.pth', map_location=device))
 model = model.to(device)
 model.eval()
 
